8f7998a47c06c6c9c2c573771136678accf9f9a2
Benjamin Cronin <bcronin720@gmail.com>
2017-08-13 09:18:33 -0700

    dev, being smart!

--- Plugin Dev/IIpD_PartModule.cs
+++ Plugin Dev/IIpD_PartModule.cs
@@ -7,37 +7,73 @@ namespace LudicrousPropulsionSystems
 {
 	public class InfiniteImprobabilityDrive : PartModule
 	{
-		[KSPField(isPersistant = true, guiActive = true, guiName = "Warp")]
+		[KSPField(isPersistant = true, guiActive = true, guiName = WarpStatus)]
 		public bool warping = false;
 		
-		List<Part> partsInVessel = new List<Part> parts;
-		public float Tea = //Get the amount of tea in the part here
-		private bool teaAvalible;
+		public string WarpStatus()
+		{
+			if (teaAvalible)
+			{
+				return "Ready To Warp!";
+			}
+			else if (warping)
+			{
+				return "Warping";
+			}
+			else if (!teaAvalible)
+			{
+				return "Warp Unavalible";
+			}
+			else
+			{
+				return "Warped!"
+			}
+		}
+		private double time()
+		{
+			return Planetarium.time;
+		}
+		private double warpedTime;
+		private bool waiting = false;
+		private double amountNeededForWarp = //constant here, how much tea is consumed per warp
+		public double Tea()
+		{
+			return FinePrint.Utilities.VesselUtilites.VesselResourceAmount(Tea, ActiveVessel);
+		}
+		public bool teaAvalible(amountNeededForWarp)
+		{
+			if (Tea >= amountNeededForWarp)
+				return true;
+			else
+				return false;
+		}
 		public void Part.Awake()
 		{
-			
+			warping = false;
+		}
+		public void UpdateWarpStatus()
+		{
+			WarpStatus();
 		}
 		public void Part.FixedUpdate()
 		{
 			if (HighLogic.LoadedSceneIsFlight)
 			{
+				UpdateWarpStatus();
 				if (/*parts detatch or explode*/)
 				{
 					//checkTeaStatus
 				}
+				if (WarpStatus == "Warped" && !waiting)
+				{
+					warpedTime = time();
+					waiting = true;
+				}
+				if (waiting)
+				{
+					if (warpedTime == (time - ))
+				}
 			}
-		}
-		private void TeaStatus(int amountRequired)
-		{
-			
-				if( && Tea >= amountRequiredForWarp)
-				{
-					teaAvalible = true;
-				}
-				else
-				{
-					teaAvalible = false;
-				}
 		}
 	}
 	public class WarpingDrive : VesselModule
