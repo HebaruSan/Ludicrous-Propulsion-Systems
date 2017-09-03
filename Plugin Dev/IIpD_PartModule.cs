001b33181711a5c805a78a25d103bd4946e77cc5
Benjamin Cronin <bcronin720@gmail.com>
2017-09-03 13:47:38 -0700

    added semimajor axis finding function for getting maxAltitude

--- Plugin Dev/IIpD_PartModule.cs
+++ Plugin Dev/IIpD_PartModule.cs
@@ -22,10 +22,10 @@ namespace LudicrousPropulsionSystems
 		private bool waiting = false;
 		private bool teaAvalible = false;
 		private Vessel ves;
-		private double amountNeededForWarp = 10; //constant here, how much tea is consumed per warp, will change to include cfg file amounts
+		private double amountNeededForWarp = 10; //TODO, constant here: how much tea is consumed per warp, change to include cfg file amounts
 		public double Tea()
 		{
-			return FinePrint.Utilities.VesselUtilities.VesselResourceAmount(Tea, FlightGlobals.ActiveVessel);//NOT METHOD, REMOVE
+			return FinePrint.Utilities.VesselUtilities.VesselResourceAmount(Tea, FlightGlobals.ActiveVessel);
 		}
 		public void TeaAvalible()
 		{
@@ -97,95 +97,103 @@ namespace LudicrousPropulsionSystems
 	public class WarpingDrive : VesselModule
 	{
 		/*
-		public class OrbGen()
+		//All to generate a crypto int, not really nessesary, slow, but fun!
+		//private RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();//Constructor for CryptoInt
+		*/
+		private bool generated = false;
+		private int planetPick;
+		private System.Random rand = new System.Random();//This is constructor for System.Random(used for .Next and .NextDouble)
+		private CelestialBody chosenPlanet;
+		/*
+		//Crypto Int Generator
+		private int GenNum(int min, int max)
 		{
-			//All to generate a crypto int, not really nessesary, slow, but fun!
-			//Should we remove this for speed issues?
-			//private RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();//Constructor for CryptoInt
-			*/
-			private bool generated = false;
-			private int planetPick;
-			private System.Random rand = new System.Random();//This is constructor for System.Random(used for .Next and .NextDouble)
-			private CelestialBody chosenPlanet;
+			uint scale = uint.MaxValue;
+			while (scale == uint.MaxValue)
+			{
+				byte[] four_bytes = new byte[4];
+				rand.GetBytes(four_bytes);
+				scale = BitConverter.ToUInt32(four_bytes, 0);
+			}
+			return (int)(min + (max - min) * (scale / (double)uint.MaxValue));
+		}
+		//End crypto int
+		*/
+		private int GenNum(int min, int max)
+		{
+			return rand.Next(min, max);
+		}
+		private double GenDouble(double min, double max)//Not crypto, but we dont really need that here
+		{
+			return rand.NextDouble() * (max - min) + min;
+		}
+		private double CBRT(double num)
+		{
+			return (System.Math.Pow(num, (1.0/3.0)));
+		}
+		public void CreatePlanetList()
+		{
+			CelestialBody sun = new CelestialBody();
+			Planetarium planetarium = new Planetarium();
+			sun = planetarium.Sun;
+			List<CelestialBody> cbE = new List<CelestialBody>();
+			cbE.Add(sun);
+			List<CelestialBody> cbU = cbE[0].orbitingBodies;
+			while(cbU.Count > 0)
+			{
+				cbU.AddRange(cbU[0].orbitingBodies);
+				cbE.Add(cbU[0]);
+				cbU.RemoveAt(0);
+			}
+		}
+		private void PlanetPick()
+		{
+			if (!generated)
+				CreatePlanetList();
+			planetPick = GenNum(1, cbE.Count);
+		}
+		private void ChoosePlanet()
+		{
+			for (int z = 0; z < cbE.Count; z++)
+			{
+				if (z == planetPick)
+				{
+					chosenPlanet = cbE[z];
+				}
+			}
+		}
+		private double MaxAlt()
+		{
 			/*
-			//Crypto Int Generator
-			private int GenNum(int min, int max)
-			{
-				uint scale = uint.MaxValue;
-				while (scale == uint.MaxValue)
-				{
-					byte[] four_bytes = new byte[4];
-					rand.GetBytes(four_bytes);
-					scale = BitConverter.ToUInt32(four_bytes, 0);
-				}
-				return (int)(min + (max - min) * (scale / (double)uint.MaxValue));
-			}
-			//End crypto int
+			Hill Sphere is a method of calculating SOI. This is done by getting the radius through the calculation r = a(1 - e)CBRT(m/3M)
+			Where a = semimajor axis, e = eccentricity, m = mass(child body), and M = mass(parent body)
 			*/
-			private int GenNum(int min, int max)
+			
+			private double obtsMA;
+			private double obtEcc;
+			private double childMass;
+			private double parentMass;
+			
+			Orbit cbOrbit = new Orbit();
+			cbOrbit = chosenPlanet.GetOrbit();
+			obtsMA = cbOrbit.semiMajorAxis;
+			obtEcc = cbOrbit.eccentricity;
+			childMass = chosenPlanet.Mass;
+			parentMass = chosenPlanet.referenceBody.Mass;
+			
+			return (a*(1-obtEcc)*CBRT(childMass/(3*parentMass)));
+		}
+		private double MinAlt()
+		{
+			if (chosenPlanet.atmosphere)
 			{
-				return rand.Next(min, max);
+				return (chosenPlanet.atmosphereDepth + 1000);
 			}
-			private double GenDouble(double min, double max)//Not crypto, but we dont really need that here
+			else
 			{
-				return rand.NextDouble() * (max - min) + min;
+				return (chosenPlanet.Radius + 1000);
 			}
-			public void CreatePlanetList()
-			{
-				CelestialBody sun = new CelestialBody();
-				Planetarium planetarium = new Planetarium();
-				sun = planetarium.Sun;
-				List<CelestialBody> cbE = new List<CelestialBody>();
-				cbE.Add(sun);
-				List<CelestialBody> cbU = cbE[0].orbitingBodies;
-				while(cbU.Count > 0)
-				{
-					cbU.AddRange(cbU[0].orbitingBodies);
-					cbE.Add(cbU[0]);
-					cbU.RemoveAt(0);
-				}
-			}
-			private void PlanetPick()
-			{
-				if (!generated)
-					CreatePlanetList();
-				planetPick = GenNum(1, cbE.Count);
-			}
-			private void ChoosePlanet()
-			{
-				for (int z = 0; z < cbE.Count; z++)
-				{
-					if (z == planetPick)
-					{
-						chosenPlanet = cbE[z];
-					}
-				}
-			}
-			private double MaxAlt()
-			{
-				//need to get CB's semimajor axis, mass, parentBody's mass
-				//return chosenPlanet.GetSOI;//FIX, does not exist
-				//TODO calculate SOI using "hill sphere" methods?
-				/*
-				Hill Sphere is a method of calculating SOI. This is done by getting the radius through the calculation r = a(1 - e)CBRT(m/3M)
-				Where a = semimajor axis, e = eccentricity, m = mass(child body), and M = mass(parent body)
-				
-				Orbit cbOrbit = new Orbit();
-				cbOrbit = chosenPlanet.GetOrbit();
-				
-				*/
-			}
-			private double MinAlt()
-			{
-				if (chosenPlanet.atmosphere)
-				{
-					return (chosenPlanet.atmosphereDepth + 1000);
-				}
-				else
-				{
-					return (chosenPlanet.Radius + 1000);
-				}
-			}
+		}
 		public void OnFixedUpdate()
 		{
 			if (!generated)
